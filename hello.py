a5d8b2eb856e76fa9d81d0848b135ba59787a6fc
dan <danielnjoo2@gmail.com>
2017-07-06 01:13:56 -0700

    Adjusted post format

--- hello.py
+++ hello.py
@@ -15,13 +15,13 @@ def hello():
 
 	soup = BeautifulSoup(r.text, "html.parser")  # BeautifulSoup(r.text, "lxml")
 
-	now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+	now = datetime.now()
 	posts = [
 		{
 			'uid': 'EXAMPLE_CHANNEL_MULTI_ITEM_JSON_TTS_{index}'.format(index=index),
-			'updateDate': now,
+			'updateDate': (now - timedelta(seconds=index)).strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
 			'titleText': post.h3.get_text(),
-			'mainText': "\n".join([p.get_text() for p in post.find(class_='blurb-content').find_all('p', recursive=False)]), 
+			'mainText': "The headline is: " + post.h3.get_text() + "The story is: " + "\n".join([p.get_text() for p in post.find(class_='blurb-content').find_all('p', recursive=False)]), 
 			'redirectUrl': post.h3.a['href']
 		} 
 		for index, post in enumerate(soup(class_="daily-blurb"))
@@ -33,7 +33,3 @@ if __name__ == "__main__":
 
 
 
-# json_response=json.dumps(r.json())
-# response=Response(json_response,content_type='application/json; charset=utf-8')
-# from flask import jsonify
-# https://stackoverflow.com/questions/25398218/getting-json-response-using-requests-object-flask
