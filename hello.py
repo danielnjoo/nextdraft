9c8d0c1e4fd0b06e6639ab777f519f4ce51ef40c
dan <danielnjoo2@gmail.com>
2017-07-06 01:50:04 -0700

    Fixed 5 headlines only

--- hello.py
+++ hello.py
@@ -21,12 +21,12 @@ def hello():
 			'uid': 'EXAMPLE_CHANNEL_MULTI_ITEM_JSON_TTS_{index}'.format(index=index),
 			'updateDate': (now - timedelta(seconds=index)).strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
 			'titleText': post.h3.get_text(),
-			'mainText': "The headline is: " + post.h3.get_text() + "The story is: " + "\n".join([p.get_text() for p in post.find(class_='blurb-content').find_all('p', recursive=False)]), 
+			'mainText': "The headline is: " + post.h3.get_text() + ". The story is: " + "\n".join([p.get_text() for p in post.find(class_='blurb-content').find_all('p', recursive=False)]), 
 			'redirectUrl': post.h3.a['href']
 		} 
 		for index, post in enumerate(soup(class_="daily-blurb"))
 	]
-	return json.dumps(posts[:5])
+	return json.dumps(posts)
 
 if __name__ == "__main__":
 	app.run()
